c5b9ed752526a15784bc4c013e7509e58e1cc073
Courtney Bryce Hilton <courtney.bryce.hilton@gmail.com>
2022-01-01 19:42:03 -0500

    Initial submission done
    Still need to clean up code a bit...

--- code/day1.R
+++ code/day1.R
@@ -25,15 +25,49 @@ rand_points <- function(n, mean_x, mean_y, sd_x, sd_y) {
     )
 }
 
-# 2.  Generate data -------------------------------------------------------
+point_creator <- function(n, mean_x = 0, mean_y = 0, sd_x = 1, sd_y = 1, colors) {
+  rand_points(n, mean_x, mean_y, sd_x,sd_y) %>%
+    mutate(
+      # add point sizes
+      size = sample(seq(0.5,10,.5), n(), replace = TRUE),
+      # add point colors
+      color = sample(colors, n(), replace = TRUE)
+    )
+}
 
-data <- rand_points(
-  n = 100,
-  mean_x = 0,
-  mean_y = 0,
-  sd_x = 1,
-  sd_y = 1
-)
 
+# aRt creation -----------------------------------------------------------
+
+
+
+for (i in 1:10) {
+  colors <- rand_color(20)
+  data <- point_creator(n = 3500, sd_x = 5, sd_y = 5, colors = colors)
+  data4 <- point_creator(n = 800, sd_x = 3, sd_y = 3, colors = colors)
+  data5 <- point_creator(n = 2000, sd_x = 1.6, sd_y = 8, colors = colors)
+  data6 <- point_creator(n = 1000, sd_x = 5, sd_y = 5, colors = colors)
+  data7 <- point_creator(n = 2500, sd_x = 4, sd_y = 4, colors = colors)
+  data2 <- point_creator(n = 100, colors = colors)
+  data3 <- point_creator(n = 99, colors = colors)
+  
+  output <- ggplot() + 
+    geom_point(data = tibble(x = 0, y = 0), aes(x,y), size = 120, color = "black") + #rand_color(1)
+    geom_point(data = data5, mapping = aes(x,y, size = size / 2.5, color = color)) + 
+    geom_point(data = data2, mapping = aes(x*2.5,y*2.5, size = size / 3, color = color)) +
+    geom_point(data = data3, mapping = aes(x*2.5,y*2.5, size = size / 3, color = color)) +
+    geom_point(data = data, mapping = aes(x,y, size = size / 20, color = color)) + 
+    geom_point(data = data7, mapping = aes(x,y, size = size / 10, color = color)) + 
+    geom_point(data = data6, mapping = aes(x,y, size = size / 8, color = color)) + 
+    scale_size_identity() +
+    scale_color_identity() +
+    scale_fill_identity() +
+    theme_void() + 
+    theme(
+      plot.background = element_rect(color = "black", fill = "white")
+    )
+  
+  ggsave(filename = paste0("day1_", i, ".png"), path = here("viz"), plot = output,
+         device = "png", width = 8, height = 8, dpi = 300)
+}
 
 
